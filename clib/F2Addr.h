bf7e82aedfcdd7ad6efe588575540bf7f6879b82
phoe-nix <wanwei1116@sina.com>
2019-07-29 20:11:02 +0800

    Added random item sets for f2.

--- clib/F2Addr.h
+++ clib/F2Addr.h
@@ -164,7 +164,7 @@ const int F2_HostDifficulty = 0x206020CA;
 //slot Scenario 62DDF6 62E324
 //slot num 63C56A 63C56C
 //slot1 628da0 +2players +4maxplayers slot2+15C
-//slot1 628DAE 00=Busy 01=Vacant 02=Busy(…Ë∂®÷–) 03=Join in 04=Full 05=playing 06=opening
+//slot1 628DAE 00=Busy 01=Vacant 02=Busy(ËÆæÂÆö‰∏≠) 03=Join in 04=Full 05=playing 06=opening
 //slot2 628EFC
 //62B672 num of players in door
 //63C56A num of players in chat room
@@ -268,13 +268,14 @@ int F2_GetLobbyAddress(int slotNum)
     }
 }
 
-const int F2_PassUB1 = 0x20490073;//“ÏΩÁ
-const int F2_PassUB2 = 0x20490075;//“ÏΩÁ
-const int F2_Pass4 = 0x20491268;//Õª∆∆
+const int F2_PassUB1 = 0x20490073;//ÂºÇÁïå
+const int F2_PassUB2 = 0x20490075;//ÂºÇÁïå
+const int F2_Pass4 = 0x20491268;//Á™ÅÁ†¥
 const int F2_Difficulty = 0x2049135A;
 const int F2_DTGasRandom = 0x2024ADB0;//24ADB0(offline) 491338(online)
 const int F2_DTGasFlag = 0x2048FFFC;
 const int F2_DTGasTime = 0x20491278;
+const int F2_ItemRandom = 0x2024AD30;//
 /*clear 4912A0
 //random set? 24AD30 397B80 397B82 491338 491353 49135E
 //gas address
